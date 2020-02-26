26042bdf96ca9f1d60ddc1e9fa4243e5788d96a8
AlmaMEriksson <eriksson.alma@gmail.com>
2020-02-26 11:30:18 +0100

    UI

---
+++ Assets/UI/CityWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CityWindow : EditorWindow
+{
+    public static void ShowWindow()
+    {
+        GetWindow<CityWindow>("City Editor");
+    }
+
+    void OnGUI()
+    {
+
+    }
+
+}
---
+++ Assets/UI/RoadWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RoadWindow : EditorWindow
+{
+    public static void ShowWindow()
+    {
+        GetWindow<RoadWindow>("Raod Editor");
+    }
+
+
+    void OnGUI()
+    {
+        
+    }
+}
