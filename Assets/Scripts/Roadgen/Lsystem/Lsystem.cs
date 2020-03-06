ba0ba87d1860f4a85816e2faba9bf19dd95a0900
Ludvig Liljeqvist <43001318+ludlil@users.noreply.github.com>
2020-03-06 14:38:16 +0100

    Påbörjat utformning av Lsystem

---
+++ Assets/Scripts/Roadgen/Lsystem/Lsystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A model for generating roads through the use of one or more 'turtles'
+/// that move along the terrain.
+/// </summary>
+public class Lsystem : MonoBehaviour
+{
+    // F -> Draw a line
+    // G -> Move forward
+    // + -> Turn left
+    // - -> Turn Right
+    // [ -> Save state
+    // ] -> Restore state
+
+    /// <summary>
+    /// The rules dictate the rewriting process.
+    /// </summary>
+    class Rule{
+        char input;
+        string output;
+        
+        Rule(char c, string s){
+            input = c;
+            output = s;
+        }
+    }
+    /// <summary>
+    /// Represents the current state of the turtle(s)
+    /// </summary>
+    class State
+    {
+        Vector3 pos;
+        Vector3 direction;
+    }
+    /// <summary>
+    /// A point in space that connects roads
+    /// </summary>
+    class Node
+    {
+        Vector3 pos;
+        Node prev;
+        HashSet<Node> next = new HashSet<Node>();
+    }
+
+    public char axiom = 'F';
+    public Set<String> ruleset = new HashSet<Rule>();
+    StringBuffer tree = new StringBuffer();
+}
