35947fce47b03622d66fc86841b6280332cf5324
David Hall <david.emanuel.hall@gmail.com>
2020-03-13 00:07:49 +0100

    Updated the DocFX documentation

--- Assets/Scripts/Extensions/CollectionExtensions.cs
+++ Assets/Scripts/Extensions/CollectionExtensions.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace Extensions
 {
     /// <summary>
-    /// Extensions for classes implementing ICollection or ICollection<T>.
+    /// Extensions for classes implementing ICollection or ICollection&lt;T&gt;.
     /// </summary>
     public static class CollectionExtensions   
     {
