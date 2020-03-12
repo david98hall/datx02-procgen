09cd26c0c9584fd7a40b393356898083052a9b26
ludhul <ludhul@student.chalmers.se>
2020-03-12 17:12:02 +0100

    A* initial

---
+++ Assets/Scripts/Cities/Roads/AStarGenerator.cs
@@ -0,0 +1,14 @@
+using Interfaces;
+
+namespace Cities.Roads
+{
+    public class AStarGenerator : IGenerator<RoadNetwork>
+    {
+        
+        public RoadNetwork Generate()
+        {
+            
+            throw new System.NotImplementedException();
+        }
+    }
+}
