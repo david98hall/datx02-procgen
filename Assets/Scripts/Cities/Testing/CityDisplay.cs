e0ff2e978a89dc35c73bc277da4fce96e6fe0add
David Hall <david.emanuel.hall@gmail.com>
2020-03-12 23:39:14 +0100

    Roads now look like roads

--- Assets/Scripts/Cities/Testing/CityDisplay.cs
+++ Assets/Scripts/Cities/Testing/CityDisplay.cs
@@ -1,48 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Cities.Roads;
-using Interfaces;
 using UnityEngine;
-using Utils;
 
 namespace Cities.Testing
 {
-    [Serializable]
-    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
     public class CityDisplay : MonoBehaviour
     {
         private readonly CityGenerator _cityGenerator;
-        private readonly HashSet<GameObject> roadRenderers;
-        public enum RoadStrategy
-        {
-            Sample, 
-            AStar,
-        }
 
-        public MeshFilter meshFilter;
-        public MeshRenderer meshRenderer;
-        public RoadStrategy roadStrategy;
-        public TerrainUtil.HeightMapInjector.MapType heightMapType;
-        public int width;
-        public int depth;
-        public int scale;
+        private readonly HashSet<GameObject> roadRenderers;
+
+        public Material roadMaterial;
         
         public CityDisplay()
         {
-            _cityGenerator = new CityGenerator();
+            _cityGenerator = new CityGenerator
+            {
+                RoadNetworkStrategy = new RoadNetworkStrategyFactory(null).CreateSampleStrategy()
+            };
             roadRenderers = new HashSet<GameObject>();
         }
         
         public void GenerateCity()
         {
-            var heightMapInjector = new TerrainUtil.HeightMapInjector 
-                {Width = width, Depth = depth, Type = heightMapType};
-
-            meshFilter.sharedMesh = TerrainUtil.Mesh(heightMapInjector.Get(), scale);
-            meshRenderer.sharedMaterial.mainTexture = Texture2D.redTexture;
-            
-            _cityGenerator.RoadNetworkStrategy = GetRoadStrategy();
             var city = _cityGenerator.Generate();
             DisplayCity(city);
         }
@@ -65,13 +46,23 @@ namespace Cities.Testing
                 var item = new GameObject("Road " + roadCount++);
                 var roadRenderer = item.AddComponent<LineRenderer>();
                 roadRenderers.Add(item);
+                
                 // Set the appearance of the road
                 roadRenderer.startWidth = 0.3f;
                 roadRenderer.endWidth = 0.3f;
+                roadRenderer.numCornerVertices = 90;
+                roadRenderer.numCapVertices = 90;
+                roadRenderer.textureMode = LineTextureMode.Tile;
+                roadRenderer.generateLightingData = true;
+                if (roadMaterial != null)
+                {
+                    roadRenderer.sharedMaterial = roadMaterial;
+                }
+                
                 // Add the vertices of the road
-                var roadVertices = road.ToArray();
-                roadRenderer.positionCount = roadVertices.Length;
-                roadRenderer.SetPositions(roadVertices);
+                var roadArray = road.ToArray();
+                roadRenderer.positionCount = roadArray.Length;
+                roadRenderer.SetPositions(roadArray);
             }
             
         }
@@ -89,28 +80,6 @@ namespace Cities.Testing
             }
             roadRenderers.Clear();
         }
-
-        private IGenerator<RoadNetwork> GetRoadStrategy()
-        {
-            
-            var heightMapInjector = new TerrainUtil.HeightMapInjector
-            {
-                Width = width, 
-                Depth = depth, 
-                Type = heightMapType
-            };
-            
-            switch (roadStrategy)
-            {
-                case RoadStrategy.Sample:
-                    return new RoadNetworkStrategySample(heightMapInjector);
-                case RoadStrategy.AStar:
-                    var aStar =  new AStarGenerator(heightMapInjector);
-                    aStar.Add((0, 0), (width / 2, depth / 2));
-                    return aStar;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+        
     }
 }
