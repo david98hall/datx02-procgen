a754644d65e9f9ca88cc9461dcb8ed0a6f3a2dcd
David Hall <david.emanuel.hall@gmail.com>
2020-03-20 00:25:25 +0100

    Trying to create triangular plots within road cycles
    Not working yet. It probably only works for undirected graphs and RoadNetwork is directed. I have to be able to convert RoadNetwork to a undirected graph.

--- Assets/Scripts/Cities/Plots/PlotStrategySample.cs
+++ Assets/Scripts/Cities/Plots/PlotStrategySample.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Cities.Roads;
 using Extensions;
 using Interfaces;
 using UnityEngine;
+using Utils.Geometry;
 
 namespace Cities.Plots
 {
@@ -18,14 +20,68 @@ namespace Cities.Plots
         {
             var plots = new HashSet<Plot>();
             
-            // TODO
+            var roadParts = RoadNetwork.GetRoadParts().ToList();
+
+            plots.AddRange(GenerateTrianglePlots(roadParts));
+
+            var plotStrings = plots.Select(plot =>
+            {
+                const string arrow = " -> ";
+                var plotString = plot.Vertices.Aggregate("Plot: ", (current, vector) => current + (vector + arrow));
+                return plotString.Substring(0, plotString.Length - arrow.Length);
+            });
+
+            Debug.Log("Number of plots: " + plots.Count);
+            
+            foreach (var plotString in plotStrings)
+            {
+                Debug.Log(plotString);
+            }
             
             return plots;
         }
 
-        private static IEnumerable<Plot> GenerateTrianglePlots(IEnumerable<(Vector3 start, Vector3 end)> roadParts)
+        private IEnumerable<Plot> GenerateTrianglePlots(IReadOnlyCollection<(Vector3 Start, Vector3 End)> roadParts)
         {
-            return null;
+            var plots = new HashSet<Plot>();
+            var vertexPairs = 
+                RoadNetwork.RoadVertices.Zip(RoadNetwork.RoadVertices, (v1, v2) => (v1, v2));
+
+            var potentialTriangleEdges = new HashSet<(Vector3 Start, Vector3 End)>();
+
+            foreach (var (Start1, End1) in vertexPairs)
+            {
+                var saveSegment = true;
+                
+                foreach (var (Start2, End2) in roadParts)
+                {
+                    var isIntersection = Maths3D.LineSegmentIntersection(
+                            out var intersection, Start1, End1, Start2, End2);
+                    saveSegment &= !isIntersection || intersection.Equals(Start2) || intersection.Equals(End2);
+                }
+
+                if (saveSegment)
+                {
+                    potentialTriangleEdges.Add((Start1, End1));
+                }
+            }
+            
+            foreach (var (Start, End) in potentialTriangleEdges)
+            {
+                foreach (var endNeighbour in RoadNetwork.GetAdjacentVertices(End))
+                {
+                    foreach (var endNeighbourNeighbour in RoadNetwork.GetAdjacentVertices(endNeighbour))
+                    {
+                        if (!RoadNetwork.IsAdjacent(endNeighbourNeighbour, Start)) continue;
+                        // Triangle found
+                        var trianglePlot = new Plot();
+                        trianglePlot.SetShapeVertices(new []{Start, End, endNeighbour, Start});
+                        plots.Add(trianglePlot);
+                    }
+                }
+            }
+            
+            return plots;
         }
         
         /*
