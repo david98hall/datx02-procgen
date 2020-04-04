1fa70399ed3eabba5eb6a6f82c9353030c905559
David Hall <david.emanuel.hall@gmail.com>
2020-04-04 18:14:48 +0200

    Clockwise: May be close to a solution

--- Assets/Scripts/Cities/Plots/ClockwiseCycleStrategy.cs
+++ Assets/Scripts/Cities/Plots/ClockwiseCycleStrategy.cs
@@ -23,14 +23,14 @@ namespace Cities.Plots
 
         public override IEnumerable<Plot> Generate()
         {
-            // DebugStuff();
+            DebugStuff();
             return GetMinimalCyclesXz().Select(cycle => new Plot(cycle));
         }
 
         private static void DebugStuff()
         {
             Debug.Log(Vector2.SignedAngle(new Vector2(1, 0), new Vector2(0, 1)));
-            Debug.Log(Vector2.SignedAngle(new Vector2(1, 0), new Vector2(0, -1)));
+            Debug.Log(Vector2.SignedAngle(new Vector2(0, 1), new Vector2(1, -1)));
         }
         
         private IEnumerable<IReadOnlyCollection<Vector3>> GetMinimalCyclesXz()
@@ -47,10 +47,13 @@ namespace Cities.Plots
             var visitedEdges = new HashSet<(Vector3 Start, Vector3 End)>();
             foreach (var vertex in roadNetwork.RoadVertices)
             {
+                Debug.Log("New cycle search: ");
                 if (TryFindCycle(vertex, roadNetwork, visitedEdges, out var cycle))
                 {
                     cycles.Add(cycle);
+                    Debug.Log("Found the cycle!");
                 }
+                Debug.Log("---------------------------------------------------");
             }
             
             return cycles;
@@ -62,30 +65,31 @@ namespace Cities.Plots
             ICollection<(Vector3 Start, Vector3 End)> visitedEdges,
             out IReadOnlyCollection<Vector3> cycle)
         {
-            cycle = FindCycle(vertex, vertex, roadNetwork, visitedEdges);
+            cycle = FindCycle(vertex, vertex, Vector3.zero, roadNetwork, visitedEdges);
             return cycle != null;
         }
 
         private static IReadOnlyCollection<Vector3> FindCycle(
             Vector3 start,
-            Vector3 vertex, 
+            Vector3 vertex,
+            Vector3 previous,
             RoadNetwork roadNetwork, 
             ICollection<(Vector3 Start, Vector3 End)> visitedEdges,
             bool firstIteration = true)
         {
             if (!firstIteration && start.Equals(vertex))
                 return new []{vertex};
-            
-            foreach (var neighbour in GetNeighboursClockwise(vertex, roadNetwork))
+
+            foreach (var neighbour in GetNeighboursClockwise(vertex, previous, roadNetwork))
             {
                 var edge = (vertex, neighbour);
-                if (visitedEdges.Contains(edge) || visitedEdges.Contains((neighbour, vertex))) continue;
-                
+                if (visitedEdges.Contains(edge)) continue;
+
                 // Mark the edge as visited
                 visitedEdges.Add(edge);
                 
                 // Find the end of the cycle (if there is one)
-                var cycleEnd = FindCycle(start, neighbour, roadNetwork, visitedEdges, false);
+                var cycleEnd = FindCycle(start, neighbour, vertex, roadNetwork, visitedEdges, false);
                 
                 if (cycleEnd != null)
                 {
@@ -104,28 +108,39 @@ namespace Cities.Plots
         private static Vector2 Vec3ToVec2(Vector3 v) => new Vector2(v.x, v.z);
         
         private static IEnumerable<Vector3> GetNeighboursClockwise(
-            Vector3 vertex, 
+            Vector3 vertex,
+            Vector3 previous,
             RoadNetwork roadNetwork)
         {
             var neighbours = new HashSet<(Vector3 Vertex, float Angle)>();
             
-            var vector = Vec3ToVec2(vertex);
+            var vertexXz = Vec3ToVec2(vertex);
+            var direction = (vertexXz - Vec3ToVec2(previous)).normalized;
             foreach (var neighbour in roadNetwork.GetAdjacentVertices(vertex))
             {
-                var neighbourVector = Vec3ToVec2(neighbour) - vector;
-                var neighbourAngle = Vector2.SignedAngle(Vector3.right, neighbourVector);
-                if (-90 < neighbourAngle && neighbourAngle < 90)
-                {
-                    neighbours.Add((neighbour, neighbourAngle));
-                }
+                var angle = Vector2.SignedAngle(direction, Vec3ToVec2(neighbour));
+                //if (angle <= 0)
+                //{
+                    neighbours.Add((neighbour, angle));
+                //}
+                Debug.Log((vertexXz, direction, Vec3ToVec2(neighbour), angle));
             }
             
             var clockwiseNeighbours = new List<(Vector3 Vertex, float Angle)>(neighbours);
             clockwiseNeighbours.Sort((vertex1, vertex2) =>
             {
-                var clockwiseDiff1 = Math.Abs(vertex1.Angle + 90);
-                var clockwiseDiff2 = Math.Abs(vertex2.Angle + 90);
-                return clockwiseDiff1 < clockwiseDiff2 ? -1 : clockwiseDiff1 > clockwiseDiff2 ? 1 : 0;
+                // Debug.Log("Magnitudes: " + (vertex1.Vertex, vertex1.Vertex.magnitude) + " + " + (vertex2.Vertex, vertex2.Vertex.magnitude));
+
+                var (vector1, angle1) = vertex1;
+                var (vector2, angle2) = vertex2;
+                
+                if (angle1 < angle2)
+                    return -1;
+                if (angle1 == angle2)
+                    return vector1.magnitude < vector2.magnitude ? -1 : vector1.magnitude > vector2.magnitude ? 1 : 0;
+                return 1;
+                
+                // return vertex1.Angle < vertex2.Angle ? -1 : vertex1.Angle > vertex2.Angle ? 1 : 0;
             });
             
             return clockwiseNeighbours.Select(tuple => tuple.Vertex);
