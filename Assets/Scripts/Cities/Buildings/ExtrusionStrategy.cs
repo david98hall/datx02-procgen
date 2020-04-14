3cc44f5c50d935a73867008eb06d53e7cc684122
dauhie <dauh.me@gmail.com>
2020-04-14 19:34:27 +0200

    initial buildings commit

---
+++ Assets/Scripts/Cities/Buildings/ExtrusionStrategy.cs
@@ -0,0 +1,263 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+using System.Linq;
+using Cities.Plots;
+using Utils.Geometry;
+using Interfaces;
+
+/// <summary>
+/// Generator for buildings. 
+/// Will use strategy to construct building according to lot shape and population density (NYI).
+/// Some plots are suitable for multiple buildings; these should be split into street-bordering 
+/// buildings and a central green area. This is done by a Lot generator.
+/// </summary>
+public class ExtrusionStrategy : Strategy<float[,], ICollection<Building>>
+{
+    private IEnumerator plots;
+
+    private float[,] heatMap;
+    private float[,] heightMap;
+
+    private float minArea;
+
+    private ICollection<Building> buildings;
+
+
+    internal ExtrusionStrategy(IInjector<float[,]> heightmapInjector, float minArea) : base(heightmapInjector)
+    {
+        this.minArea = minArea;
+        heightMap = heightmapInjector.Get();
+        buildings = new List<Building>();
+    }
+
+    internal void AddPlots(IEnumerator plots)
+    {
+        this.plots = plots;
+    }
+
+    /// <summary>
+    /// Generate all buildings in the given set of plots.
+    /// </summary>
+    /// <returns>The generated buildings.</returns>
+    public override ICollection<Building> Generate()
+    {
+
+        while (plots.MoveNext())
+        {
+            if (plots.Current != null)
+            {
+                LotGenerator lg = new LotGenerator( (Plot) plots.Current, 0);
+                ICollection<Lot> lots = lg.Generate();
+
+                buildings = GetBuildings((IList<Lot>)lots);
+            }
+        }
+
+        return buildings;
+    }
+
+    /// <summary>
+    /// Generate all buildings in a set of lots.
+    /// </summary>
+    /// <param name="lots">The lots to generate a building in.</param>
+    /// <returns></returns>
+    public ICollection<Building> GetBuildings(ICollection<Lot> lots)
+    {
+        IList<Building> buildings = new List<Building>();
+
+        foreach (Lot lot in lots)
+        {
+            // Only generate building if suitable lot
+            if (ValidLot(lot))
+            {
+                //float y = heightMap[Mathf.RoundToInt(lot.center.x), Mathf.RoundToInt(lot.center.z)];
+                float y = Random.Range(1f, 5.5f);
+                float marginSize = 0.5f;
+
+                IList<Vector3> vertices = (IList<Vector3>) PolyOps.ShrinkPoly(lot.Vertices.ToList(), marginSize);
+
+                // Polygon centroid
+                Vector2 c = Maths2D.GetConvexCenter(ToXZ(vertices));
+                Vector3 center = new Vector3(c.x, 0, c.y);
+
+                vertices.RemoveAt(vertices.Count - 1);
+                var tup = SetBaseHeight(vertices);
+
+                var (verts, tris) = ExtrudePolygon(vertices, tup.max + y);
+                Mesh m = BuildMesh(verts, tris);
+
+                buildings.Add(new Building(center, Vector2.zero, m));
+            }
+        }
+
+        return buildings;
+    }
+
+    private bool ValidLot(Lot lot)
+    {
+        bool valid = false;
+
+        if (lot.area >= minArea)
+            valid = true;
+
+        return valid;
+    }
+
+    /// <summary>
+    /// Finds the height of the terrain for each building base vertex, as well as the maximum height for 
+    /// extruding the top into a flat shape suitable for more floors. (May be unnecessary with min height)
+    /// </summary>
+    /// <param name="poly">The polygonal 2D shape of the building.</param>
+    /// <returns>The y-translated shape and the maximum height.</returns>
+    private (IList<Vector3> vertices, float max) SetBaseHeight(IList<Vector3> poly)
+    {
+        float max = float.MinValue;
+        float min = MinimumHeightInBounds(poly);
+
+        for (int i = 0; i < poly.Count; i++)
+        {
+            int x = Mathf.FloorToInt(poly[i].x);
+            int y = Mathf.FloorToInt(poly[i].z);
+            float fx = poly[i].x - x;
+            float fy = poly[i].z - y;
+
+            float height1 = Mathf.Lerp(heightMap[x, y], heightMap[x + 1, y], fx);
+            float height2 = Mathf.Lerp(heightMap[x, y + 1], heightMap[x + 1, y + 1], fx);
+            float finalHeight = Mathf.Lerp(height1, height2, fy);
+
+            max = Mathf.Max(finalHeight, max);
+
+            poly[i] = new Vector3(poly[i].x, min, poly[i].z);
+        }
+        return (poly, max);
+    }
+
+    private float MinimumHeightInBounds(IList<Vector3> poly)
+    {
+        foreach (var p in poly)
+            Debug.Log(p.x + ", " + p.z);
+        var (minX, minY, maxX, maxY) = Maths2D.GetExtremeBounds(ToXZ(poly));
+
+        int MinX = Mathf.FloorToInt(minX);
+        int MaxX = Mathf.CeilToInt(maxX);
+        int MinY = Mathf.FloorToInt(minY);
+        int MaxY = Mathf.CeilToInt(maxY);
+
+        float min = 0f;
+
+        for (int y = MinY; y <= MaxY; y++)
+        {
+            for (int x = MinX; x <= MaxX; x++)
+            {
+                min = Mathf.Min(min, heightMap[x, y]);
+            }
+        }
+
+        return min;
+    }
+
+    /// <summary>
+    ///  Uniformly extends a polygon upwards and connects the vertices on the sides.
+    /// </summary>
+    /// <param name="vertices">The points of the original 2D shape.</param>
+    /// <param name="distance">The distance to extend the polygon.</param>
+    /// <returns>A tuple of the new vertex and index list.</returns>
+    private (ICollection<Vector3>, ICollection<int>) ExtrudePolygon(IList<Vector3> vertices, float distance)
+    {
+        //// determine winding order
+        //bool ccw = Maths2D.PointsAreCounterClockwise(ToXZ(vertices));
+
+        // Prepare a shape for extrusion (using triangulator to get tris)
+        Triangulator t = new Triangulator(new Polygon(ToXZ(vertices).ToArray(), new Vector2[0][]));
+        int[] bottomTris = t.Triangulate();
+
+        ICollection<int> tris = new List<int>();
+        tris.AddRange(bottomTris);
+
+        // Add indices and points for the additional vertex array (which has same relative indices as bottom)
+        int n = vertices.Count;
+        for (int i = 0; i < bottomTris.Length; i++)
+        {
+            tris.Add(bottomTris[i] + n);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            vertices.Add(new Vector3(vertices[i].x, distance, vertices[i].z));
+        }
+
+        return ConstructSideFaces(vertices, tris);
+    }
+
+    /// <summary>
+    /// Constructs a simple face of 4 vertices for each edge, connecting the extruded polygon to the base one.
+    /// </summary>
+    /// <param name="verts">The full list of vertices.</param>
+    /// <param name="tris">The full list of triangles.</param>
+    /// <returns>The provided vertices and triangles with added sides.</returns>
+    private (ICollection<Vector3>, ICollection<int>) ConstructSideFaces(IList<Vector3> verts, ICollection<int> tris)
+    {
+        int n = (verts.Count / 2);
+        int j = verts.Count;
+        int i11 = n - 1;
+        int i21 = j - 1;
+        int i22 = n;
+
+        for (int i12 = 0; i12 < n; i11 = i12++)
+        {
+            // Triangle one
+            verts.Add(verts[i12]);
+            verts.Add(verts[i11]);
+            verts.Add(verts[i22]);
+
+            tris.Add(j++);
+            tris.Add(j++);
+            tris.Add(j++);
+
+            // Triangle two
+            verts.Add(verts[i21]);
+
+            tris.Add(j - 1);
+            tris.Add(j - 2);
+            tris.Add(j++);
+            i21 = i22++;
+        }
+
+        return (verts, tris);
+    }
+
+
+    /// <summary>
+    /// Constructs a mesh out of the given vertices and triangles.
+    /// </summary>
+    /// <param name="verts">The list of points to add to the mesh.</param>
+    /// <param name="tris">The indices for the vertex array.</param>
+    /// <returns>The mesh constructed from the arrays.</returns>
+    private Mesh BuildMesh(ICollection<Vector3> verts, ICollection<int> tris)
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts.ToArray();
+        mesh.triangles = tris.ToArray();
+        mesh.uv = new Vector2[verts.Count];
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+
+    #region Utility methods
+
+    private List<Vector2> ToXZ(IEnumerable<Vector3> vecs)
+    {
+        List<Vector2> vecsXZ = new List<Vector2>();
+
+        foreach (Vector3 v in vecs)
+        {
+            vecsXZ.Add(v.ToXZ());
+        }
+        return vecsXZ;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Cities/Buildings/LotGenerator.cs
@@ -0,0 +1,288 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cities.Plots;
+using Utils.Geometry;
+using System.Linq;
+using Extensions;
+
+/// <summary>
+/// Generates lots from plots through subdivision. 
+/// The lots will then hold buildings placed within their lots according to parameters.
+/// </summary>
+/// 
+public class LotGenerator
+{
+    internal float splitOffset;
+    internal float minSize = 10f;
+    internal float maxRatio = 2f;
+    internal float maxSide;
+    internal float minSide;
+
+    private readonly float boundsFactor;
+    private readonly bool clockwise;
+    private int mode;
+
+    private readonly Plot plot;
+    private readonly ICollection<Lot> lots = new List<Lot>();
+    //private HashSet<Vector3> accessPoints;
+
+
+    public LotGenerator(Plot plot, int mode)
+    {
+        this.plot = plot;
+
+        var verts = plot.Vertices;
+        var vertsXZ = ToXZ(verts);
+
+        clockwise = Maths2D.PointsAreCounterClockwise(vertsXZ);
+
+        var bounds = Maths2D.GetExtremeBounds(vertsXZ);
+        boundsFactor = new Vector2(bounds.MaxX - bounds.MinX, bounds.MaxY - bounds.MinY).sqrMagnitude;
+
+        //accessPoints = new HashSet<Vector3>(verts);
+        this.mode = mode;
+    }
+
+    /// <summary>
+    /// Generates lots from plots, depending on chosen method.
+    /// </summary>
+    /// <returns>A collection of the lots obtained from subdivision.</returns>
+    public ICollection<Lot> Generate()
+    {
+        lots.Clear();
+
+        var verts = plot.Vertices;
+        var poly = new List<Vector3>();
+        poly.AddRange(verts);
+
+        switch (mode)
+        {
+            case 0: BisectorDivide(poly); break;
+            case 1: OffsetDivide(poly, 2f); break;
+        }
+
+        return lots;
+    }
+
+    #region Offset lot division
+
+    /// <summary>
+    /// Subdivides a plot by shrinking its borders and then splitting margin according to given parameters.
+    /// </summary>
+    /// <param name="mainLot"></param> The main lot.
+    /// <param name="offset"></param> The amount to offset inwards.
+    private void OffsetDivide(List<Vector3> mainLot, float offset)
+    {
+        var center = Maths2D.GetCenterPoint(ToXZ(mainLot));
+
+        //// Old for reference
+        //lots.Add(new Lot(poly, new Vector3(center.x, 0, center.y)));
+
+        IList<Vector3> innerBorder = (IList<Vector3>) PolyOps.ShrinkPoly(mainLot, offset);
+
+        SplitMargin(mainLot, innerBorder);
+    }
+
+    // TODO: not working
+    private void SplitMargin(IList<Vector3> outerBorder, IList<Vector3> innerBorder)
+    {
+        float length = 1f;
+        IList<Vector3> pairs = new List<Vector3>();
+
+        for (int i = 1; i < innerBorder.Count; i++)
+        {
+            var inner1 = innerBorder[i - 1];
+            var inner2 = innerBorder[i];
+            var outer1 = outerBorder[i - 1];
+            var outer2 = outerBorder[i];
+            var dir = inner2 - inner1;
+
+            var c = Vector3.Cross(Vector3.up, dir.normalized) * 10;
+
+            var line1 = (dir * (0.5f) + inner1) - c; // point j * length units along inner border
+            var line2 = line1 + (c*2); // orthogonal line from inner border outwards
+
+            if (Maths3D.LineSegmentIntersection(out var intersectionIn, inner1, inner2, line1, line2))
+                pairs.Add(intersectionIn);
+            else if (Maths3D.LineSegmentIntersection(out var intersectionOut, outer1, outer2, line1, line2))
+                pairs.Add(intersectionOut);
+
+                //for (int j = 1; j < Mathf.RoundToInt(dir.magnitude); j++)
+                //{
+                //    var line1 = dir.normalized * (length * j) + inner1; // point j * length units along inner border
+                //    var line2 = line1 + c; // orthogonal line from inner border outwards
+
+                //    if (Maths3D.LineSegmentIntersection(out var intersectionIn, inner1, inner2, line1, line2))
+                //        pairs.Add(intersectionIn);
+                //    else if (Maths3D.LineSegmentIntersection(out var intersectionOut, outer1, outer2, line1, line2))
+                //        pairs.Add(intersectionOut);
+                //}
+        }
+
+        for (int i = 3; i < pairs.Count; i+=2)
+        {
+            lots.Add(new Lot(new List<Vector3> {pairs[i-3], pairs[i-2], pairs[i], pairs[i-1], pairs[i-3]}, Vector3.zero));
+        }
+    }
+
+    #endregion
+
+    #region Bisector lot division
+
+    /// <summary>
+    /// Recursively split a polygonal shape along its bisector.
+    /// </summary>
+    /// <param name="poly">The polygonal shape to split.</param>
+    private void BisectorDivide(IList<Vector3> poly)
+    {
+        var baseVars = FindBaseVariables(poly);
+
+        if (TerminationConditions(baseVars.area, baseVars.ratio))
+        {
+            var center = Maths2D.GetCenterPoint(ToXZ(poly));
+            lots.Add(new Lot(poly, new Vector3(center.x, 0, center.y)));
+        }
+        else
+        {
+            var newLots = SplitAtEdge(poly, baseVars.index);
+
+            BisectorDivide(newLots.Item1);
+            BisectorDivide(newLots.Item2);
+        }
+    }
+
+    /// <summary>
+    /// Checks base cases for recursion
+    /// </summary>
+    /// <param name="area">Area of polygon.</param>
+    /// <param name="ratio">Side ratio of polygon.</param>
+    /// <returns>Continue or not.</returns>
+    private bool TerminationConditions(float area, float ratio)
+    {
+        return area < minSize || ratio > maxRatio;
+    }
+
+
+    /// <summary>
+    /// Split a polygon along the bisector of the longest edge. Works for convex polygons and concave polygons
+    /// for which the bisector only exits the polygon once.
+    /// </summary>
+    /// <param name="poly">The polygon to split.</param>
+    /// <param name="index">The index at which to split.</param>
+    /// <returns></returns>
+    private (IList<Vector3>, IList<Vector3>) SplitAtEdge(IList<Vector3> poly, int index)
+    {
+
+        var indices = new List<int>();
+        var intersections = new Dictionary<int, Vector3>();
+
+        var prev = poly[index];
+        var next = poly[(index + 1) % poly.Count];
+        var nextDir = (next - prev);
+
+        // Random halfway point along the edge (should be multiplied by a block type factor; smaller interval yields more rectangular shapes)
+        // TODO: RANDOM NUMBER OFFSETS POSITIONS FOR SOME REASON
+        Random.InitState(123);
+        float f = Random.Range(0.25f, 0.75f);
+        var splitPoint = (nextDir * 0.5f ) + prev;
+
+        // Perpendicular line direction, scaled outside bounds
+        var splitVector = (clockwise ? Vector3.Cross(nextDir, Vector3.up) : Vector3.Cross(Vector3.up, nextDir)).normalized;
+        splitVector = splitVector * boundsFactor;
+
+        for (int i = 1; i < poly.Count; i++)
+        {
+            if (Maths3D.LineSegmentIntersection(out var intersection, poly[i - 1], poly[i], splitPoint - splitVector, splitVector + splitPoint))
+            {
+                if (!intersections.ContainsValue(intersection))
+                {
+                    intersections.Add(i - 1, intersection);
+                    indices.Add(i - 1); // ordered
+                }
+            }
+        }
+
+        // Should not happen
+        if (intersections.Count < 2)
+        {
+            throw new UnityException("Not enough intersections when splitting lot");
+        }
+
+        // Get new polygons with intersections
+        var poly1 = new List<Vector3>();
+        var poly2 = new List<Vector3>();
+
+        int start = indices[0];
+        int end = indices[1];
+
+        for (int i = 0; i < poly.Count; i++)
+        {
+            if (i <= start || i > end)
+                poly1.Add(poly[i]);
+            else
+                poly2.Add(poly[i]);
+        }
+
+        poly1.Insert(start + 1, intersections[indices[1]]);
+        poly1.Insert(start + 1, intersections[indices[0]]);
+
+        poly2.Add(intersections[indices[1]]);
+        poly2.Add(intersections[indices[0]]);
+        poly2.Add(poly[start + 1]);
+
+        return (poly1, poly2);
+    }
+
+    /// <summary>
+    /// Finds variables for recursive split, as well as the index at which the polygon should split.
+    /// </summary>
+    /// <param name="poly">The polygon to split.</param>
+    /// <returns>The split index, area and side ratio of the polygon</returns>
+    private (int index, float area, float ratio) FindBaseVariables(IList<Vector3> poly)
+    {
+        int splitIndex = 0;
+        float max = (poly[1] - poly[0]).magnitude;
+        float min = max;
+        float area = 0;
+
+        for (int i = 1; i < poly.Count; i++)
+        {
+            float f = (poly[i] - poly[i - 1]).magnitude;
+
+            if (f > max)
+            {
+                max = f;
+                splitIndex = i - 1;
+            }
+            else
+            {
+                min = Mathf.Min(min, f);
+            }
+
+            float a = poly[i - 1].x * poly[i].z;
+            float b = poly[i - 1].z * poly[i].x;
+            area += (a - b);
+        }
+
+        var (MinX, MinY, MaxX, MaxY) = Maths2D.GetExtremeBounds(ToXZ(poly));
+
+        var bmax = (Mathf.Max((MaxX - MinX), (MaxY - MinY)));
+        var bmin = (Mathf.Min((MaxX - MinX), (MaxY - MinY)));
+
+        return (splitIndex, Mathf.Abs(area/2), bmax / bmin);
+    }
+
+    #endregion
+
+    private List<Vector2> ToXZ(IEnumerable<Vector3> vecs)
+    {
+        List<Vector2> vecsXZ = new List<Vector2>();
+
+        foreach (Vector3 v in vecs)
+        {
+            vecsXZ.Add(v.ToXZ());
+        }
+        return vecsXZ;
+    }
+}
